af8797c9b140210bca38fbeb28b0a3ada2e33a2a
Simun-12 <simunsarangi0912@gmail.com>
2025-03-31 18:18:21 +0530

    init

---
+++ snapcat/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Snapcat</h1>
+      <h2>Snapcat is a simple app to take photos of your catss</h2>
+    </div>
+  );
+}
+
+export default Home;
+
